abe173a3626ee99edda95a976756fc69f769e9e4
maske <maske@Shubh>
2022-10-18 19:59:55 +0530

    Git 1st code

---
+++ src/demo_Pack/Demo_Class.java
@@ -0,0 +1,10 @@
+package demo_Pack;
+
+public class Demo_Class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("1st code for git");
+	}
+
+}
